5968e566de849fc509a7dbaf08db93c4959bdc3d
Mateusz Skoczek <mateusz5432160@gmail.com>
2024-09-24 15:18:15 +0200

    test2

--- WatchIt.Database/WatchIt.Database.Model/WatchIt.Database.Model.Seeding/DataReader.cs
+++ WatchIt.Database/WatchIt.Database.Model/WatchIt.Database.Model.Seeding/DataReader.cs
@@ -11,8 +11,8 @@ public class DataReader
     public static IEnumerable<T> Read<T>(string filename)
     {
         string jsonFile = $@"{AppContext.BaseDirectory}/WatchIt.Database/WatchIt.Database.Model/WatchIt.Database.Model.Seeding/Data/{filename}.json";
-        Console.WriteLine(string.Join(';', Directory.GetFiles($@"{AppContext.BaseDirectory}")));
-        Debug.WriteLine(string.Join(';', Directory.GetFiles($@"{AppContext.BaseDirectory}")));
+        Console.WriteLine(string.Join('\n', Directory.GetDirectories($@"{AppContext.BaseDirectory}")));
+        Debug.WriteLine(string.Join('\n', Directory.GetDirectories($@"{AppContext.BaseDirectory}")));
         string dataString = File.ReadAllText(jsonFile);
         IEnumerable<T>? data = JsonSerializer.Deserialize<IEnumerable<T>>(dataString);
         if (data is null)
