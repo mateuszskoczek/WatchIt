7d4266d3ea11f013ddbfede635987db69f9ee1d4
Mateusz Skoczek <mateusz5432160@gmail.com>
2024-09-24 15:00:40 +0200

    json path changed

--- WatchIt.Database/WatchIt.Database.Model/WatchIt.Database.Model.Seeding/DataReader.cs
+++ WatchIt.Database/WatchIt.Database.Model/WatchIt.Database.Model.Seeding/DataReader.cs
@@ -9,7 +9,7 @@ public class DataReader
     public static IEnumerable<T> Read<T>() => Read<T>(typeof(T).Name);
     public static IEnumerable<T> Read<T>(string filename)
     {
-        string jsonFile = $@"../../WatchIt.Database/WatchIt.Database.Model/WatchIt.Database.Model.Seeding/Data/{filename}.json";
+        string jsonFile = $@"{AppContext.BaseDirectory}/WatchIt.Database/WatchIt.Database.Model/WatchIt.Database.Model.Seeding/Data/{filename}.json";
         string dataString = File.ReadAllText(jsonFile);
         IEnumerable<T>? data = JsonSerializer.Deserialize<IEnumerable<T>>(dataString);
         if (data is null)
