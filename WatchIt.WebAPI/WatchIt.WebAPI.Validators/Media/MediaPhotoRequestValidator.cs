d02207a33534aca7105667212a5a4c5f2704acd5
Mateusz Skoczek <mateusz5432160@gmail.com>
2024-09-25 22:24:15 +0200

    photo validators fixed

--- WatchIt.WebAPI/WatchIt.WebAPI.Validators/Media/MediaPhotoRequestValidator.cs
+++ WatchIt.WebAPI/WatchIt.WebAPI.Validators/Media/MediaPhotoRequestValidator.cs
@@ -2,19 +2,19 @@
 using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
 using WatchIt.Common.Model.Media;
 using WatchIt.Database;
+using WatchIt.WebAPI.Validators.Photos;
 
 namespace WatchIt.WebAPI.Validators.Media;
 
 public class MediaPhotoRequestValidator : AbstractValidator<MediaPhotoRequest>
 {
-    public MediaPhotoRequestValidator(DatabaseContext database)
+    public MediaPhotoRequestValidator()
     {
         RuleFor(x => x.Image).NotEmpty();
         RuleFor(x => x.MimeType).Matches(@"\w+/.+").WithMessage("Incorrect mimetype");
         When(x => x.Background is not null, () =>
         {
-            RuleFor(x => x.Background!.FirstGradientColor).Must(x => x.Length == 3).WithMessage("First gradient color has to be 3 byte long");
-            RuleFor(x => x.Background!.SecondGradientColor).Must(x => x.Length == 3).WithMessage("Second gradient color has to be 3 byte long");
+            RuleFor(x => x.Background!).SetValidator(new PhotoBackgroundDataValidator());
         });
     }
 }
