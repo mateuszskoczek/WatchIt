f5a72d7a83238df6b19bf36f802eb302d246261c
Mateusz Skoczek <mateusz5432160@gmail.com>
2024-10-13 12:10:52 +0200

    actor roles panel added

--- WatchIt.Website/WatchIt.Website/Components/MediaPage/ActorRolesPanelComponent.razor.cs
+++ WatchIt.Website/WatchIt.Website/Components/MediaPage/ActorRolesPanelComponent.razor.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Components;
-using WatchIt.Common.Model.Roles;
-using WatchIt.Website.Services.Utility.Configuration;
 using WatchIt.Website.Services.WebAPI.Media;
 
 namespace WatchIt.Website.Components.MediaPage;
@@ -20,38 +18,5 @@ public partial class ActorRolesPanelComponent : ComponentBase
     [Parameter] public string Class { get; set; } = string.Empty;
     [Parameter] public required long Id { get; set; }
     
-    #endregion
-    
-    
-    
-    #region FIELDS
-
-    private bool _loaded;
-
-    private IEnumerable<ActorRoleResponse> _roles = [];
-
-    #endregion
-
-    
-    
-    #region PUBLIC METHODS
-
-    protected override async Task OnAfterRenderAsync(bool firstRender)
-    {
-        List<Task> endTasks = new List<Task>();
-        
-        // STEP 0
-        endTasks.AddRange(
-        [
-            MediaWebAPIService.GetMediaAllActorRoles(Id, successAction: data => _roles = data)
-        ]);
-        
-        // END
-        await Task.WhenAll(endTasks);
-        
-        _loaded = true;
-        StateHasChanged();
-    }
-
     #endregion
 }
---
+++ WatchIt.Website/WatchIt.Website/Components/MediaPage/RoleComponent.razor.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Components;
+
+namespace WatchIt.Website.Components.MediaPage;
+
+public partial class RoleComponent : ComponentBase
+{
+    #region PARAMETERS
+    
+    [Parameter] public string? AdditionalName { get; set; }
+    
+    #endregion
+    
+    
+}
